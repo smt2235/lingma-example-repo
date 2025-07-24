1b17e75385d919515ae1c5f7ea229337cd5a531d
smt2235 <46770935+smt2235@users.noreply.github.com>
2025-07-24 18:15:12 +0800

    Add HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
